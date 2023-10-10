d1c63172a5a5623ec89b1ce2fe3bb831269757ac
Vitalii <144382079+VitaliiOrel@users.noreply.github.com>
2023-10-10 22:31:13 +0200

    Add files via upload

--- src/route/index.js
+++ src/route/index.js
@@ -12,22 +12,25 @@ router.get('/', function (req, res) {
 router.get('/ex1', function (req, res) {
   res.render('prod-cont', {
     layout: 'product',
+    title: 'Відповідь №1',
     product: {
       name: 'Корм для риб',
       cost: 72.5,
       img: 'fish.png',
+      desc: 'Корм для донних рибок. Вага нетто 25 г.',
+      isAvailable: true,
     },
   })
 })
 
 router.get('/ex2', function (req, res) {
-  res.render('prod-cont', {
-    layout: 'product',
-    title: 'Product',
-    product: {
-      name: 'Корм для риб',
-      cost: 72.5,
-      img: 'fish.png',
+  res.render('user-cont', {
+    layout: 'user',
+    title: 'Відповідь №2',
+    user: {
+      name: 'Іван',
+      email: 'ivan@example.com',
+      phone: '123-456-7890',
     },
   })
 })
@@ -35,11 +38,13 @@ router.get('/ex2', function (req, res) {
 router.get('/ex3', function (req, res) {
   res.render('prod-cont', {
     layout: 'product',
-    title: 'Product',
+    title: 'Відповідь №3',
     product: {
       name: 'Корм для риб',
       cost: 72.5,
       img: 'fish.png',
+      desc: 'Корм для донних рибок. Вага нетто 25 г.',
+      isAvailable: true,
     },
   })
 })
@@ -47,35 +52,40 @@ router.get('/ex3', function (req, res) {
 router.get('/ex4', function (req, res) {
   res.render('prod-cont', {
     layout: 'product',
-    title: 'Product',
+    title: 'Відповідь №4',
     product: {
       name: 'Корм для риб',
       cost: 72.5,
       img: 'fish.png',
+      desc: 'Корм для донних рибок. Вага нетто 25 г.',
+      isAvailable: false,
     },
   })
 })
 
 router.get('/ex5', function (req, res) {
-  res.render('prod-cont', {
+  res.render('prod-cont-unless', {
     layout: 'product',
-    title: 'Product',
+    title: 'Відповідь №5',
     product: {
       name: 'Корм для риб',
       cost: 72.5,
       img: 'fish.png',
+      desc: 'Корм для донних рибок. Вага нетто 25 г.',
+      isAvailable: false,
     },
   })
 })
 
 router.get('/ex6', function (req, res) {
-  res.render('prod-cont', {
-    layout: 'product',
-    title: 'Product',
-    product: {
-      name: 'Корм для риб',
-      cost: 72.5,
-      img: 'fish.png',
+  res.render('user-cont', {
+    layout: 'user',
+    title: 'Відповідь №6',
+    user: {
+      name: 'Іван',
+      email: 'ivan@example.com',
+      phone: '123-456-7890',
+      balance: 0,
     },
   })
 })
